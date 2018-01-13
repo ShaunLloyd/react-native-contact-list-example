c7437ca3ad78a346947183020315276f76ba5c92
shaun lloyd <shaunlloydmedia@gmail.com>
2018-01-13 15:36:40 -0500

    [ISSUE-1]: Rename application to Composed Contacts - Regenerate android and ios projects - Relink native packages - Update react-native-navigation to latest

---
+++ android/app/src/main/java/com/composedcontacts/MainActivity.java
@@ -0,0 +1,5 @@
+package com.composedcontacts;
+
+import com.reactnativenavigation.controllers.SplashActivity;
+
+ public class MainActivity extends SplashActivity {}
---
+++ android/app/src/main/java/com/composedcontacts/MainApplication.java
@@ -0,0 +1,35 @@
+package com.composedcontacts;
+
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.react.ReactPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+  @Override
+  public boolean isDebug() {
+     return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public String getJSMainModuleName() {
+      return "index";
+  }
+
+  protected List<ReactPackage> getPackages() {
+     return Arrays.<ReactPackage>asList(
+         new VectorIconsPackage(),
+         new ReactNativeI18n(),
+         new LinearGradientPackage()
+     );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+     return getPackages();
+  }
+}
